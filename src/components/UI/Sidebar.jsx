5c83d479df4b93214f26061fe50c20da9b9be2b0
Cyril-vtn <votion.cyril@gmail.com>
2023-02-05 11:36:44 +0100

    Fonts added & reponsive homepage

--- src/components/UI/Sidebar.jsx
+++ src/components/UI/Sidebar.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import classes from './Sidebar.module.css'
+import logo from '../../assets/logoGreen.svg'
 
-const Sidebar = () => {
-  return <div className={classes.Sidebar}></div>;
+const Sidebar = ({children}) => {
+  return <div className={classes.sidebar}>
+    <div className={classes.logo}>
+      <Link to='/learn'>
+        <img src={logo} alt="" />
+      </Link>
+      </div>
+      {children}</div>
 };
 
 export default Sidebar;
